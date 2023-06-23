0c911eac51080cd58f9573f9f115c37125c9f8fd
naitelkadir <naitelkadir@gmail.com>
2023-06-23 08:38:36 -0700

    tesgt

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -21,7 +21,11 @@ void swap_ints(int *a, int *b)
 void shell_sort(int *array, size_t size)
 {
 	size_t gap, i, j;
-
+	
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
 	for (gap = 1; gap < size / 3;)
 	{
 		gap = gap * 3 + 1;
